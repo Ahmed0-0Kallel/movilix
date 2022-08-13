bad3960904c7e0f0811984fc2d8e2f7273c4f05b
Ahmed Kallel <contactahmedkallel0@gmail.com>
2022-08-13 14:28:59 +0100

    Project Structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes} from 'react-router-dom';
 
-function App() {
-  <div>
-    App
+import useStyles from './styles'
 
-  </div>;
-}
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './'
+const App = () => {
+  const classes = useStyles();
+return (
+<div className={classes.root} >
+  <CssBaseline/> 
+  <NavBar  />
+  <main className={classes.content}> 
+  <div className={classes.toolbar} />
+      <Routes>
+        <Route  exact path="/movie/:id"    element={  <MovieInformation/>  }/>  
+        <Route  exact path="/actors/:id"  element={ <Actors/> }/>
+        <Route  exact path="/"            element={ <Movies/> } />
+        <Route  exact path="/profile/:id" element= { <Profile/> }/>
+      </Routes>
+  </main>
+
+</div>
+  )
+};
 
 export default App;
